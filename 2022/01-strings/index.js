9c47fdcbd6849bf49bca54def7d4ec302b60a961
geraldiner <hello@geraldiner.com>
2022-10-05 20:06:43 -0400

    day 5

--- 2022/01-strings/index.js
+++ 2022/01-strings/index.js
@@ -102,6 +102,63 @@ const input4 = "   The white space at the beginning of this string doesn't match
 const revInput4 = reverseString(input4);
 // console.log({revInput4});
 
+/* DAY 5 */
+const input5 = "The Pawshank Redemption,Caturday Night Live,Only Meworders in the Building,I Love Mewcy, Mewsummer Meowders,The Golden Purrs, Purrlandia ,Meowpardy, Meowstery Science Theater: Purrthousand, Amewican Idol,Dog City,Doctor Mew , The Meowing Fed,Mew Peter,The Vicar of Dogley, Kittens,Meownton Abbey,Pets and the Kitty,Dogis and Bonehead,Pawlty Meowers ,The Meowpet Show,Barkos,The Catbert Purrport,The Pawffice,The Dogford Files, Battlestar Catlactica,Catlumbo,SpongeDog Squarepants,NYPD Mew ,Fluffy the Meowpire Purrer,The Inbemewners,Meowder She Wrote,Paw & Order,30 Dog, Pawvatar: The Last Meowbender,The Pawnight Show,Arrested Dogvelopment,Furiends,Mewie,Curb Your Dogthusiasm,Teenage Mewtant Ninja Turtles,Phineas and Purrb,Paw Trek, Paw Trek: The Next Mewination, Twin Mewks, *C*A*T*S*,DogTales, Game of Bones, House of the Meowgon,The Purrlight Zone,Breaking Bone,The Meowre,The Dogpranos,The Rings of Meower, The KIT Crowd,Strangepaw Things ,Catman: The Animeowted Series,Meowter Call Saul,Mewgerton ,Obark,Mewphoria,La Casa de Pawpel,Rick & Meowty,Amewican Purror Story, Mewcifer,PawndaVision,Dogxter,The Meowndalorian, Dog Lasso,Bark,Meowdern Pawmily , Meowtlander,Bone Mirror,Barks and Recreation,How to Get Away with Meowder,Boneland ,Meowther Ted,Mewtopia,Mewey,The Mewkie Meowse Doghouse,Mewster Rogers' Neighborhood";
+const puns = input5.split(",");
+
+const isValidCatPun = (pun) => {
+	if (pun[0] === ' ' || pun[pun.length-1] === ' ') {
+		return false;
+	}
+	if (pun.toLowerCase().includes("dog") || pun.toLowerCase().includes("bark") || pun.toLowerCase().includes("bone")) {
+		return false;
+	}
+	if (pun.length % 5 === 0) {
+		return false;
+	}
+	if (pun.charCodeAt(0) + pun.charCodeAt(pun.length-1) % 2 === 0) {
+		return false;
+	}
+	if (pun[Math.round(pun.length/2)] === 'e') {
+		return false;
+	}
+	if (countLowerCaseLetters(pun) % 2 === 1) {
+		return false;
+	}
+	if (countUpperCaseLetters(pun) < 2) {
+		return false;
+	}
+	if (pun.includes("S")) {
+		return false;
+	}
+	return true;
+}
+
+const countLowerCaseLetters = (word) => {
+	const lowerCaseLettersCount = word.split("").reduce((acc, curr) => {
+		if (curr.charCodeAt(0) >= 97 && curr.charCodeAt(0) <= 122) {
+			return acc+1;
+		} else {
+			return acc;
+		}
+	})
+	return lowerCaseLettersCount;
+}
+
+const countUpperCaseLetters = (word) => {
+	const upperCaseeLettersCount = word.split("").reduce((acc, curr) => {
+		if (curr.charCodeAt(0) >= 65 && curr.charCodeAt(0) <= 90) {
+			return acc+1;
+		} else {
+			return acc;
+		}
+	})
+	return upperCaseeLettersCount;
+}
+
+const validPuns = puns.filter(isValidCatPun);
+console.log(validPuns);
+
 const secretMessage = "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs";
 
 console.log(secretMessage);
@@ -114,3 +171,4 @@ console.log(spacesAddedSecretMessage);
 console.log("reversing string...");
 const reversedSecretMessage = reverseString(spacesAddedSecretMessage);
 console.log(reversedSecretMessage);
+console.log("validating...");
