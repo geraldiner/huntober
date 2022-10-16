b3fc6cdb4f819f7bc6c730e90525d4088078cf85
geraldiner <hello@geraldiner.com>
2022-10-16 10:20:39 -0400

    fix up array methods

--- 2022/02-arrays/index.js
+++ 2022/02-arrays/index.js
@@ -49,40 +49,46 @@ const compareArrays = (arrA, arrB) => {
 const moveElementLeft = (arr, el) => {
     const elIndex = arr.indexOf(el);
     if (elIndex - 1 < 0) {
-        return;
+        return arr;
     }
     const prevElement = arr[elIndex - 1];
-    arr.splice(elIndex-1, 1, el);
-    arr.splice(elIndex, 1, prevElement);
+	arr[elIndex - 1] = el;
+	arr[elIndex] = prevElement;
+    // arr.splice(elIndex-1, 1, el);
+    // arr.splice(elIndex, 1, prevElement);
+	return arr;
 }
 const moveElementRight = (arr, el) => {
     const elIndex = arr.indexOf(el);
     if (elIndex + 1 > arr.length-1) {
-        return;
+        return arr;
     }
-    const prevElement = arr[elIndex + 1];
-    arr.splice(elIndex+1, 1, el);
-    arr.splice(elIndex, 1, prevElement);
+    const nextElement = arr[elIndex + 1];
+	arr[elIndex + 1] = el;
+	arr[elIndex] = nextElement;
+	return arr;
+    // arr.splice(elIndex+1, 1, el);
+    // arr.splice(elIndex, 1, nextElement);
 }
-const arr5 = ['abc', 'xyz', 1, 2, 'Hey!']
+let arr5 = ['abc', 'xyz', 1, 2, 'Hey!']
 
 // call move left function with 'xyz' and arr5 as arguments
-moveElementLeft(arr5, 'xyz');
+arr5 = moveElementLeft(arr5, 'xyz');
 // console.log(compareArrays(arr5, ['xyz', 'abc', 1, 2, 'Hey!']));
 // ['xyz', 'abc', 1, 2, 'Hey!']
 
-moveElementLeft(arr5, 1);;
+arr5 = moveElementLeft(arr5, 1);;
 // console.log(compareArrays(arr5, ['xyz', 1, 'abc', 2, 'Hey!']));
 // ['xyz', 1, 'abc', 2, 'Hey!']
 
 // call move left function again, same arguments
 // Note that 'xyz' is already as far left as it can go
-moveElementLeft(arr5, 'xyz');
+arr5 = moveElementLeft(arr5, 'xyz');
 // console.log(compareArrays(arr5, ['xyz', 1, 'abc', 2, 'Hey!']));
 // ['xyz', 1, 'abc', 2, 'Hey!'] no change
 
 // call move right function this time, with 2 and arr5 as arguments
-moveElementRight(arr5, 2);
+arr5 = moveElementRight(arr5, 2);
 // console.log(compareArrays(arr5, ['xyz', 1, 'abc', 'Hey!', 2]));
 // ['xyz', 1, 'abc', 'Hey!', 2]
 
@@ -117,16 +123,21 @@ const resultArr = moveToFront(myArr);
 const moveElementUp = (arr, el) => {
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = 0; j < arr[i].length; j++) {
-			if (arr[i][j] === el) {
+			const currRow = arr[i];
+			if (currRow[j] === el) {
 				const prevRowIndex = i-1
-				if (prevRowIndex >= 0) {
-					const prevElement = arr[prevRowIndex][j];
-					const prevRowArr = arr[prevRowIndex];
-					prevRowArr.splice(j, 1, el);
-					arr[i].splice(j, 1, prevElement);
-					arr.splice(prevRowIndex, 1, prevRowArr);
-					arr.splice(i, 1, arr[i]);
+				if (prevRowIndex < 0) {
+					return arr;
 				}
+				const prevRowArr = arr[prevRowIndex];
+				const prevElement = prevRowArr[j];
+				prevRowArr[j] = el;
+				currRow[j] = prevElement;
+				return arr;
+				// prevRowArr.splice(j, 1, el);
+				// arr[i].splice(j, 1, prevElement);
+				// arr.splice(prevRowIndex, 1, prevRowArr);
+				// arr.splice(i, 1, arr[i]);
 			}
 		}
 	}
@@ -134,46 +145,51 @@ const moveElementUp = (arr, el) => {
 const moveElementDown = (arr, el) => {
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = 0; j < arr[i].length; j++) {
-			if (arr[i][j] === el) {
+			const currRow = arr[i];
+			if (currRow[j] === el) {
 				const nextRowIndex = i+1
-				if (nextRowIndex <= arr.length-1) {
-					const nextElement = arr[nextRowIndex][j];
-					const nextRowArr = arr[nextRowIndex];
-					nextRowArr.splice(nextRowIndex, 1, el);
-					arr[i].splice(j, 1, nextElement);
-					arr.splice(nextRowIndex, 1, nextRowArr);
-					arr.splice(i, 1, arr[i]);				
+				if (nextRowIndex > arr.length - 1) {
+					return arr;
 				}
+				const nextRowArr = arr[nextRowIndex];
+				const nextElement = nextRowArr[j];
+				nextRowArr[j] = el;
+				currRow[j] = nextElement;
+				return arr;
+				// nextRowArr.splice(nextRowIndex, 1, el);
+				// arr[i].splice(j, 1, nextElement);
+				// arr.splice(nextRowIndex, 1, nextRowArr);
+				// arr.splice(i, 1, arr[i]);
 			}
 		}
 	}
-	return arr;	
+	return arr;
 }
 
-myGrid = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']];
+let myGrid = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']];
 
 // call move up function with 'h' and myGrid
-moveElementUp(myGrid, 'h');
+myGrid = moveElementUp(myGrid, 'h');
 // console.log(compareGrids(myGrid, [['a', 'b', 'c'], ['d', 'h', 'f'], ['g', 'e', 'i']]));
 // [['a', 'b', 'c'], ['d', 'h', 'f'], ['g', 'e', 'i']]
 
 // call move up function again, same arguments
-moveElementUp(myGrid, 'h');
+myGrid = moveElementUp(myGrid, 'h');
 // console.log(compareGrids(myGrid, [['a', 'h', 'c'], ['d', 'b', 'f'], ['g', 'e', 'i']]));
 // [['a', 'h', 'c'], ['d', 'b', 'f'], ['g', 'e', 'i']]
 
 // call move up function again, same arguments
 // Note that 'h' is already as far up as it can go
-moveElementUp(myGrid, 'h');
+myGrid = moveElementUp(myGrid, 'h');
 // console.log(compareGrids(myGrid, [['a', 'h', 'c'], ['d', 'b', 'f'], ['g', 'e', 'i']]));
 
 // call move down function this time, with 'f' and myGrid as arguments
-moveElementDown(myGrid, 'f');
+myGrid = moveElementDown(myGrid, 'f');
 // console.log(compareGrids(myGrid, [['a', 'h', 'c'], ['d', 'b', 'i'], ['g', 'e', 'f']]));
 // [['a', 'h', 'c'], ['d', 'b', 'i'], ['g', 'e', 'f']]
 
 // call move down function again, same arguments
 // Note that 'f' is already as far down as it can go
-moveElementDown(myGrid, 'f');
+myGrid = moveElementDown(myGrid, 'f');
 // console.log(compareGrids(myGrid, [['a', 'h', 'c'], ['d', 'b', 'i'], ['g', 'e', 'f']]));
 // [['a', 'h', 'c'], ['d', 'b', 'i'], ['g', 'e', 'f']]
